0718efd9fe6b3dfa63cf22cde60ccebc93318c97
Chiamaka Ezeirunne <fayhvor@gmail.com>
2022-06-23 09:58:58 +0100

    assignment

--- src/main/java/africa/semicolon/trueCaller/controllers/ContactController.java
+++ src/main/java/africa/semicolon/trueCaller/controllers/ContactController.java
@@ -1,35 +1,36 @@
-//package africa.semicolon.trueCaller.controllers;
-//
-//import africa.semicolon.trueCaller.services.ContactService;
-////import africa.semicolon.trueCaller.services.ContactServiceImpl;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//public class ContactController {
-//    private ContactService contactService = new ContactServiceImpl();
-//
-//    @PostMapping("/contact")
-//    public String addContact(@RequestBody RequestDto requestDto){
-//        contactService.addContact(requestDto.getFirstName(), requestDto.getLastName(),
-//                requestDto.getPhoneNumber());
-//        return "Added successfully";
-//    }
-//
-//    @GetMapping("/contact/{firstName}")
-//    public String findByFirstName(@PathVariable String firstName){
-//        return  contactService.findByFirstName(firstName).toString();
-//    }
-//}
-//
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
-//class RequestDto{
-//    private String firstName;
-//    private String lastName;
-//    private String phoneNumber;
-//
-//}
+package africa.semicolon.trueCaller.controllers;
+
+import africa.semicolon.trueCaller.services.ContactService;
+//import africa.semicolon.trueCaller.services.ContactServiceImpl;
+import africa.semicolon.trueCaller.services.ContactServiceImpl;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ContactController {
+    private ContactService contactService = new ContactServiceImpl();
+
+    @PostMapping("/contact")
+    public String addContact(@RequestBody RequestDto requestDto){
+        contactService.addContact(requestDto.getFirstName(), requestDto.getLastName(),
+                requestDto.getPhoneNumber());
+        return "Added successfully";
+    }
+
+    @GetMapping("/contact/{firstName}")
+    public String findByFirstName(@PathVariable String firstName){
+        return  contactService.findName(firstName).toString();
+    }
+}
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class RequestDto{
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+}
